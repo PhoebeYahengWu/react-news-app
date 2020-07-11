3bc07966590bd844a6c23691f9b85d9fa89f64d8
wuyaheng <wuyaheng2016@gmail.com>
2020-07-11 00:28:11 -0400

    add card image style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <nav>
+    <div className="nav-wrapper #546e7a blue-grey darken-1">
+      <a href="/" className="brand-logo center">Bitcoin Latest News</a>
+    </div>
+    </nav>
       <News />
     </div>
   );
--- src/components/News/NewSingle.js
+++ src/components/News/NewSingle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NewSingle = ({item}) => (
-    <div className="col s4">
+    <div className="col s12 m4 l3">
         <div className="card">
         <div className="card-image">
             <img src={item.urlToImage} alt={item.title}/>
